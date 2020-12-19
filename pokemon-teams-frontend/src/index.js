5a7a3ac47546258ceb385dea154fe03a0423f8ab
edmundo papi <edmundopierre@yahoo.com>
2020-12-19 01:01:04 +0000

    Automatically backed up by Learn

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded',()=> {
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const trainerContainer = document.querySelector('#trainer-container')
+let trainers = []
+console.log(trainerContainer)
+})
